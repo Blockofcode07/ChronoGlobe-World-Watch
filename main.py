ede6eb05c8f466d2cfa49a71b360beff3af9b7eb
Aditya <adityajawanjal27@gmail.com>
2025-04-15 11:24:38 +0530

    Initialized Streamlit app with basic setup

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from datetime import datetime
+import pytz
+import time
+
+# Streamlit Page Configuration and Title
+st.set_page_config(page_title="🌍 ChronoGloabe -Live World Clock", layout="centered")
